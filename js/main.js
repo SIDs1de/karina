af4e49e7c16dd51a3da94ba2eca847ba30643c60
SIDs1de <82277183+SIDs1de@users.noreply.github.com>
2021-12-31 00:15:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.mil__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    speed: 400,
+    arrows: false,
+    lazyLoad: 'ondemand',
+    rows: 2
+});
+
